81ff8df5380bed10f8e1a745ac7ba3bc09ab9b87
Joseph Reddy <josephreddy@Josephs-MacBook-Air.local>
2023-07-18 18:33:48 -0500

    added status of assigned

---
+++ src/components/RedirectToLoginPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-sheet>
+    
+  </v-sheet>
+</template>
+
+<script>
+import router from '../router'
+
+  export default {
+    data: () => ({
+      
+    }),
+
+    methods: {
+      
+    },
+    beforeMount() {
+      router.push('/login')
+    }
+  }
+
+</script>
